5064786c6033ba47e41388ebd86e3c0abeb66916
Snowball <miao.dextrad@gmail.com>
2024-09-15 14:36:37 +0800

    change logs: - fix start.sh runtime error

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 export DB_PORT=${DB_PORT:-3306}
 
 # install dnf-plugins-core
-echo "$(date +%F_%R) [New Install] install dnf-plugins-core and php-fpm - new install."
+echo "$(date +%F_%R) [New Install] install dnf-plugins-core and php-fpm mariadb-devel - new install."
 yum update -y
-yum install -y dnf-plugins-core php-fpm
+yum install -y dnf-plugins-core php-fpm mariadb-devel
 
 # set server timezone
 echo "$(date +%F_%R) [Note] Setting server timezone settings to '${TZ}'"
